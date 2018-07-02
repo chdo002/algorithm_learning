1a598f748d210b964fafa236d4853fa8fed1c09d
chdo002 <1107661983@qq.com>
2018-07-02 18:20:46 +0800

    在日志中添加版本标识

--- sf/sf/QuickSort.swift
+++ sf/sf/QuickSort.swift
@@ -10,18 +10,25 @@
 import Foundation
 
 func QuickSort(arr:inout [Int]){
-    
+    __quickSort(arr: &arr, l: 0, r: arr.count - 1)
 }
-
 
 private func __quickSort(arr:inout [Int], l: Int, r: Int){
-    
+    let p = __partition(arr: &arr, l: l, r: r)
+    if p >= 0 {
+        __quickSort(arr: &arr, l: l, r: p)
+        __quickSort(arr: &arr, l: p + 1, r: r)
+    }
 }
 
-private func __partition(arr:inout [Int], l: Int, r: Int){
-    let v = arr[l]
-    var j = l + 1
-//    while <#condition#> {
-//        <#code#>
-//    }
+// 寻找arr中，某个数字（一般第一个数）所在的位置p，使得p之前的比这数字小，p之后比这个大，
+private func __partition(arr:inout [Int], l: Int, r: Int) -> Int{
+
+    if l > r {
+        return -1
+    }
+    
+    
+    
+    return 0
 }
